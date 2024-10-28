75cc1ac8e4b4e1ce8854dfd66d74e78afba8bb73
himanidesai <de99hi@gmail.com>
2024-10-28 13:59:51 -0400

    Adding the Edit functionality

--- controllers/expenses-controller.js
+++ controllers/expenses-controller.js
@@ -39,3 +39,71 @@ export const getExpensesByYear = async(req, res) => {
     res.status(400).send(`Error getting expense item: ${error}`);
   }
 }
+
+export const getExpenseById = async(req, res) => {
+    const {id} = req.params;
+    try {
+        const expenseItem = await knex('expenses')
+        .where({id : id});
+
+        if (!expenseItem) {
+            return res.status(404).json({ message: "Expenses for "+ year +" not found." });
+        }
+
+        res.status(200).json(expenseItem);
+    }catch (error) {
+    res.status(400).send(`Error getting expense item: ${error}`);
+  }
+}
+
+//PUT/UPDATE existing item in the inventories table
+export const edit = async (req,res) => {
+    const { expense_date, user_id, bills_and_utilities, grocery_and_food, insurances, tax, investments, other_purchases} = req.body;
+  
+    if ( !expense_date || !user_id || !bills_and_utilities || !grocery_and_food || !insurances || !tax || !investments || !other_purchases ) {
+      return res.status(400).json({
+        message: "Please provide missing data"
+      });
+    }
+   
+    if (isNaN(bills_and_utilities) || Number(bills_and_utilities) < 0
+        || isNaN(grocery_and_food) || Number(grocery_and_food) < 0
+        || isNaN(insurances) || Number(insurances) < 0
+        || isNaN(tax) || Number(tax) < 0
+        || isNaN(investments) || Number(investments) < 0
+        || isNaN(other_purchases) || Number(other_purchases) < 0){
+      return res.status(400).json({
+        message: "Expense values must be a number"
+      })
+    }
+  
+    try {
+      const updatedExpense = await knex("expenses")
+        .where({ user_id: user_id })
+        .andWhere({ id : req.params.id})
+        .update({
+            bills_and_utilities,
+            grocery_and_food,
+            insurances,
+            tax,
+            investments,
+            other_purchases
+        });
+      
+      if (!updatedExpense) {
+        return res.status(404).json({
+          message: `Could not find expense: ${req.params.id}`
+        });
+      }
+  
+      const newItem = await knex("expenses")
+        .where({ id: req.params.id })
+        .first();
+      
+      res.status(200).json(newItem);
+    } catch (error) {
+      res.status(500).json({
+        message: `Error updating inventory item: ${error}`
+      });
+    }
+  };
--- routes/expenses-route.js
+++ routes/expenses-route.js
@@ -1,11 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import {getExpensesByMonth, getExpensesByYear} from '../controllers/expenses-controller.js'
+import {getExpensesByMonth, getExpensesByYear, getExpenseById, edit} from '../controllers/expenses-controller.js'
 
 router.route('/:user_id/:month/:year')
     .get(getExpensesByMonth)
 router.route('/:user_id/:year')
     .get(getExpensesByYear)
+router.route('/:id')
+    .get(getExpenseById)
+    .put(edit)
+
 
 export default router;
